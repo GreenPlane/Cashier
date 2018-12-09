950b7be5e9b2ab96e3a13aef0c39ea9c03c0d35b
Ethan Lee <40707681+GreenPlane@users.noreply.github.com>
2018-12-08 22:35:56 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,36 +22,19 @@ class Main {
 
         System.out.println("Change owed: " + change + "\n");
 
-        int noDecimals = (int)(change * 100); //Allows for calculation of individual dollars & coins.
-        int coins = 0; //Initialization
-        if (noDecimals >= 100 && noDecimals < 1000){ //If change owed is less than $10.00 but either $1 or above
-            int dollars = noDecimals / 100;
-            coins = (noDecimals - dollars * 100); //Gets rid of cash amount.
-            int quarters = coins / 25;
-            int dimes = (coins - (quarters * 25))/10;
-            int nickels = (coins - (quarters * 25 + dimes * 10))/5;
-            int pennies = (coins - (quarters * 25 + dimes * 10 + nickels * 5));
-            System.out.println("Dollars: " + dollars);
-            System.out.println("Quarters: " + quarters);
-            System.out.println("Dimes: " + dimes);
-            System.out.println("Nickels: " + nickels);
-            System.out.println("Pennies: " + pennies);
-        }
-        else if (noDecimals >= 1000 && noDecimals < 10000){ //If change owed is $10 or more, but less than $100. (Assuming customer doesn't pay with anything higher than $20 bills))
-            int dollars = noDecimals / 100;
-            coins = (noDecimals - dollars * 100); //Gets rid of cash amount.
-            int quarters = coins / 25;
-            int dimes = (coins - (quarters * 25))/10;
-            int nickels = (coins - (quarters * 25 + dimes * 10))/5;
-            int pennies = (coins - (quarters * 25 + dimes * 10 + nickels * 5));
-            System.out.println("Dollars: " + dollars);
-            System.out.println("Quarters: " + quarters);
-            System.out.println("Dimes: " + dimes);
-            System.out.println("Nickels: " + nickels);
-            System.out.println("Pennies: " + pennies);
-        }
-        else{
-            System.out.println("Pay with bills less than $100.");
-        }
+        int noDecimals = (int) (change * 100); 
+        int coins = 0; 
+
+        int dollars = noDecimals / 100;
+        coins = (noDecimals - dollars * 100);
+        int quarters = coins / 25;
+        int dimes = (coins - (quarters * 25)) / 10;
+        int nickels = (coins - (quarters * 25 + dimes * 10)) / 5;
+        int pennies = (coins - (quarters * 25 + dimes * 10 + nickels * 5));
+        System.out.println("Dollars: " + dollars);
+        System.out.println("Quarters: " + quarters);
+        System.out.println("Dimes: " + dimes);
+        System.out.println("Nickels: " + nickels);
+        System.out.println("Pennies: " + pennies);
     }
 }
