29d1e66c165c72b5ca4a29a6e9ae15bd79b95b88
Ethan Lee <40707681+GreenPlane@users.noreply.github.com>
2018-12-08 22:33:17 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,17 +20,16 @@ class Main {
 
         double change = amountPaid - totalPrices;
 
-        System.out.println("Change owed: " + change);
+        System.out.println("Change owed: " + change + "\n");
 
-        int noDecimals = (int)change * 100; //Allows for calculation of individual dollars & coins.
-
+        int noDecimals = (int)(change * 100); //Allows for calculation of individual dollars & coins.
         int coins = 0; //Initialization
         if (noDecimals >= 100 && noDecimals < 1000){ //If change owed is less than $10.00 but either $1 or above
-            coins = noDecimals - (noDecimals % 1000); //Gets rid of cash amount.
-            int dollars = noDecimals % 1000 / 100;
+            int dollars = noDecimals / 100;
+            coins = (noDecimals - dollars * 100); //Gets rid of cash amount.
             int quarters = coins / 25;
             int dimes = (coins - (quarters * 25))/10;
-            int nickels = (coins - (quarters * 25 + dimes * 10)/5);
+            int nickels = (coins - (quarters * 25 + dimes * 10))/5;
             int pennies = (coins - (quarters * 25 + dimes * 10 + nickels * 5));
             System.out.println("Dollars: " + dollars);
             System.out.println("Quarters: " + quarters);
@@ -39,11 +38,11 @@ class Main {
             System.out.println("Pennies: " + pennies);
         }
         else if (noDecimals >= 1000 && noDecimals < 10000){ //If change owed is $10 or more, but less than $100. (Assuming customer doesn't pay with anything higher than $20 bills))
-            coins = noDecimals - (noDecimals % 1000)*100; //Gets rid of cash amount.
-            int dollars = noDecimals % 1000 / 100;
+            int dollars = noDecimals / 100;
+            coins = (noDecimals - dollars * 100); //Gets rid of cash amount.
             int quarters = coins / 25;
             int dimes = (coins - (quarters * 25))/10;
-            int nickels = (coins - (quarters * 25 + dimes * 10)/5);
+            int nickels = (coins - (quarters * 25 + dimes * 10))/5;
             int pennies = (coins - (quarters * 25 + dimes * 10 + nickels * 5));
             System.out.println("Dollars: " + dollars);
             System.out.println("Quarters: " + quarters);
